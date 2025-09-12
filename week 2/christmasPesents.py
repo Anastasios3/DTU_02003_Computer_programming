81c4a79c94b748396f6f06fac8097cd9c403f47b
Anastasios3 <tatarakis.a@gmail.com>
2025-09-12 09:32:38 +0200

    Add initial implementation of Christmas presents logic

---
+++ week 2/christmasPesents.py
@@ -0,0 +1,12 @@
+month = "December"
+day = 23
+
+if (month == "December"):
+    print("Radio plays Christmas songs")
+
+    if (day == 24):
+        print("Let's open the presents!")
+    else:
+        print("No presents today.")
+else:
+    print("No Christmas music")
