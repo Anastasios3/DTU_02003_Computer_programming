4922dc65e894f98b1e43bbdacb42e08f4616abcd
Anastasios3 <tatarakis.a@gmail.com>
2025-09-12 09:21:58 +0200

    Add weather condition evaluation based on temperature

---
+++ week 2/howIsTheWeather.py
@@ -0,0 +1,10 @@
+temprature = 19
+
+if temprature < 0:
+    print("Freezing weather")
+elif (temprature >= 0) and (temprature <= 15):
+    print("It is cold")
+elif (temprature > 15) and (temprature <= 25):
+    print("It is pleasant")
+else:
+    print("It is hot")
