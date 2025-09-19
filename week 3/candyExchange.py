5054a120563e6bc90509342e4016a27c8dc77632
Anastasios3 <tatarakis.a@gmail.com>
2025-09-19 08:51:22 +0200

    Refactor candy exchange logic to initialize variables and enhance readability

--- week 3/candyExchange.py
+++ week 3/candyExchange.py
@@ -1,4 +1,21 @@
-"Bring back five candy wrappers, and you get one candy for free!"
-"How many candies can you end up eating?"
+# Bring back five candy wrappers, and you get one candy for free!
+# How many candies can you end up eating?
 
-candies = 15
+wrappers = 0
+candies = 609
+eaten = 0
+
+print("candies:", candies, "wrappers:", wrappers, "eaten:", eaten)
+
+candies = candies - 1
+wrappers = wrappers + 1
+eaten = eaten + 1
+
+while candies > 0:
+    candies = candies - 1
+    wrappers = wrappers + 1
+    eaten = eaten + 1
+    if wrappers >= 5:
+        candies = candies + 1
+        wrappers = wrappers - 5
+    print("candies:", candies, "wrappers:", wrappers, "eaten:", eaten)
