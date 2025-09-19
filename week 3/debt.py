bd3e29f769942218d16a11b296b43c26ef253939
Anastasios3 <tatarakis.a@gmail.com>
2025-09-19 11:14:19 +0200

    Add debt.py to calculate total debt repayment time and amount with interest

---
+++ week 3/debt.py
@@ -0,0 +1,24 @@
+debt = 20000
+monthly_payment = 2000
+interest_rate = 0.05
+
+total_paid = 0
+months = 0
+
+while debt > 0:
+    months += 1
+    
+    interest = interest_rate * debt
+    debt = debt + interest
+    
+    if debt <= monthly_payment:
+        payment_this_month = debt
+        debt = 0
+    else:
+        payment_this_month = monthly_payment
+        debt = debt - monthly_payment
+    
+    total_paid += payment_this_month
+
+print(f"Debt paid after {months} months.")
+print(f"Total payment {total_paid:.2f} kr.")
